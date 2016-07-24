128ba8028c688b3f07ff0d876d97d966ea7b9f8c
linesh <linesh.simpcity@gmail.com>
2016-07-25 00:56:54 +0800

    Linesh: Refactor test, make test more valuable for testing real response

--- server/src/test/java/com/thoughtworks/faq/controller/CategoryControllerTest.java
+++ server/src/test/java/com/thoughtworks/faq/controller/CategoryControllerTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class CategoryControllerTest {
@@ -15,8 +14,11 @@ public class CategoryControllerTest {
     public void should_return_json_with_all_possible_categories() throws Exception {
         CategoryController categoryController = new CategoryController();
 
-        Map<String, List<Category>> categories = categoryController.getCategories("personal-insurance");
+        Map<String, List<Category>> categoriesJson = categoryController.getCategories("personal-insurance");
+        List<Category> categories = categoriesJson.get("categories");
 
-        assertThat(categories, is(notNullValue()));
+        assertThat(categories.contains(Category.GENERIC), is(true));
+        assertThat(categories.contains(Category.HOME), is(true));
+        assertThat(categories.contains(Category.MOTOR), is(true));
     }
 }
