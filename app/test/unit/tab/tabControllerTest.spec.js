de13aca644823813c59234bb3c07ce6730f2931f
linesh <linesh.simpcity@gmail.com>
2016-07-24 21:04:09 +0800

    Linesh: Use es6 arrow function syntax

--- app/test/unit/tab/tabControllerTest.spec.js
+++ app/test/unit/tab/tabControllerTest.spec.js
@@ -1,16 +1,20 @@
 import { expect } from 'chai'
 import TabController from '../../../tab/tab-controller.js'
 
-describe('tab controller', function() {
-    describe('when initialized', function () {
+describe('tab controller', () => {
+    describe('when initialized', () => {
         let tabController;
 
-        beforeEach(function() {
+        beforeEach(() => {
             tabController = new TabController();
         });
 
-        it('should have a variable named first initialized', function () {
+        it('should have a variable named first initialized', () => {
             expect(tabController.first).to.equal('Angular up and running!')
         });
     });
+
+    describe('when functioning...', () => {
+
+    })
 });
